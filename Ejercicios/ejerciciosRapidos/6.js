6c70e50c6d48cc76a383bb54a0a9b944e21d49d1
Diego Gregorio Pérez <diego.gregorio.perez@ces-vegamedia.es>
2026-02-16 13:42:51 +0100

    Ej6Fixed

--- Ejercicios/ejerciciosRapidos/6.js
+++ Ejercicios/ejerciciosRapidos/6.js
@@ -1,13 +1,17 @@
-var num1=parseInt(prompt("Dime el primer número"));
-var num2=parseInt(prompt("Dime el segundo número"));
-var operacion=prompt("Elige la operacion (+,-,*,/)");
+do {
+    var num1=parseInt(prompt("Dime el primer número"));
+    var num2=parseInt(prompt("Dime el segundo número"));
+    var operacion=prompt("Elige la operacion (+,-,*,/)");
 
-if (operacion=="+") {
-    alert("La suma es: "+(num1+num2));
-} else if (operacion=="-") {
-    alert("La resta es: "+(num1-num2));
-} else if (operacion=="*") {
-    alert("La multiplicación es: "+(num1*num2));
-} else if (operacion=="/") {
-    alert("La división es: "+(parseFloat(num1)/num2));
-}
+    if (operacion=="+") {
+        alert("La suma es: "+(num1+num2));
+    } else if (operacion=="-") {
+        alert("La resta es: "+(num1-num2));
+    } else if (operacion=="*") {
+        alert("La multiplicación es: "+(num1*num2));
+    } else if (operacion=="/" && num2!=0) {
+        alert("La división es: "+(parseFloat(num1)/num2));
+    } else {
+        alert("Alguno de los valores no está permitido");
+    }
+}while (op!="+" && op!="-" && op!="*" && op!="/");
